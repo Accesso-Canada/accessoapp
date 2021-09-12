1e2d4398ba99170157a68ed38f3a96b1d95cbe22
alissalozhkin <65978304+alissalozhkin@users.noreply.github.com>
2021-09-12 18:09:14 -0400

    Add files via upload
    Contains title and image file for each mall

--- ReviewsPage/data/mall_info.js
+++ ReviewsPage/data/mall_info.js
@@ -1,6 +1,39 @@
 const mallData = [
-    {'title': 'Bayview Village', 'description': 'Bayview Village is a mall located in North York with over 110 stores. Â\xa0There are several stores that are quite accessible.'},
-    {'title': 'CF Shops at Don Mills', 'description': 'This outdoor mall is located in North York and has many retail stores and many restaurants. A large portion of stores do not have buttons, so we suggest shopping here with a friend.'},
-    {'title': 'East York Town Centre', 'description': 'East York Town Centre is a single floored spacious mall with many stores accessible from inside and outside the mall. However, it is difficult to access some of the stores from the outside. Unfortunately, we were not given permission to collect data from a number of stores in this mall.'},
-    
-]
+    {   'file': 'Bayview_Village_Shopping_Centre.jpg',
+        'title': 'Bayview Village'},
+
+    {   'file': 'CF-Don-Mills-2.jpg',
+        'title': 'CF Shops at Don Mills'},
+
+    {   'file': 'east-york-town-centre.jpg',
+        'title': 'East York Town Centre'},
+
+    {   'file': '20202406-Eatons_Centre-10.jpg',
+        'title': 'Eaton Centre'},
+
+    {   'file': '21-343_CF_Shops_Website_Redesign_Images6-min.jpg',
+        'title': 'Fairview Mall'},
+
+    {   'file': '1280px-Markville_Shopping_Centre_(37470730856).jpg',
+        'title': 'Markville Mall'},
+
+    {   'file': 'Promenade_2012_13-1.jpg',
+        'title': 'Promenade'},
+
+    {   'file': 'hero.jpg',
+        'title': 'Scarborough Town Centre'},
+
+    {   'file': '8375-24005.jpg',
+        'title': 'Square One'},
+
+    {   'file': 'vaughanmills-main.jpg',
+        'title': 'Vaughan Mills'},
+
+    {   'file': 'yonge-and-eglinton-inside2.jpg',
+        'title': 'Yonge Eglinton Centre'},
+
+    {   'file': '20181015_2018_2F5A8900_1465x900.jpg',
+        'title': 'Yonge Sheppard centre'},
+        
+    {   'file': 'Yorkdale_Mall.jpg',
+        'title': 'Yorkdale'}]
